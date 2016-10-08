b122e4b36ffbbc7dd957921328661eac95a1c226
Je <kkburst@gmail.com>
2016-10-08 11:08:35 +1100

    Testing post/get

--- matelook.py
+++ matelook.py
@@ -1,19 +1,39 @@
 #!/usr/local/bin/python3.5 -u
 
-from flask import Flask
+from flask import Flask, session, escape, request, url_for, redirect
 app = Flask( __name__ )
 
-@app.route( '/' )
-def hello_world( ):
-    return '<h1>Hello there bro.</h1>'
+@app.route( '/', methods=['GET', 'POST'] )
+def index( ):
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
 
-@app.route( '/user/<username>' )
-def show_user( username ):
-    return '<h2>Username: %s does not exist.' % username
+    if 'username' in session:
+        return 'Logged in as %s' % escape( session[ 'username'] )
+    else:
+        return '''
+            <form action="" method="post">
+                <label>Username</label>
+                <p><input type=text name=username>
+                <p><input type=submit value=Login>
+            </form>
+        '''
 
-@app.route( '/post/<int:post_id>' )
-def show_post( post_id ):
-    return 'Post: %d' % post_id
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        return '<h1>Sorry bicboi.</h1>'
+
+@app.route( '/logout' )
+def logout( ):
+    session.pop( 'username', None )
+    return redirect( url_for( 'index' ) )
 
 if __name__ == "__main__":
     app.run( debug=True, port=5000, host="0.0.0.0" )
+
+app.secret_key = 'yoloswag420'
