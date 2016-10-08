24ad0927eabfff67b600a05c39f7876f9e85042f
Je <kkburst@gmail.com>
2016-10-08 13:26:33 +1100

    User viewing in progress

--- matelook.py
+++ matelook.py
@@ -1,10 +1,11 @@
 #!/usr/local/bin/python3.5 -u
 
-from flask import Flask, session, escape, request, url_for, redirect
+from flask import Flask, session, escape, request, url_for, redirect, render_template
+from collections import defaultdict
 import re, sys, os, glob
 app = Flask( __name__ )
 
-users_dir = "dataset-medium"
+users_dir = "static\dataset-medium"
 
 @app.route( '/', methods=['GET', 'POST'] )
 def index( ):
@@ -26,36 +27,57 @@ def index( ):
             </form>
         '''
 
-@app.route( '/users' )
-@app.route( '/users/' )
 @app.route( '/user/<user_name>' )
 def viewUser( user_name=None ):
 
     if( user_name == None ):     # Show a random user.
-        users = sorted( glob.glob( os.path.join( users_dir, "*" ) ) )
-        u_ToShow = users[ 0 ];
-        print( "Showing " + str( u_ToShow ) )
-    else:
-        username = re.findall( r'z[0-9]{7}', user_name )    # Sanitize input
-        if not username:                                    # No user with the username found
-            return "<p>Invalid username %s</p>" % escape( user_name )
+        return "<p>Please enter a username</p>"
 
-        u_ToShow = os.path.join( users_dir, username[ 0 ] )
+    username = re.findall( r'z[0-9]{7}', user_name )    # Sanitize input
+    if not username:                                    # No user with the username found
+        return "<p>Invalid username %s</p>" % escape( user_name )
 
+    u_ToShow = os.path.join( users_dir, username[ 0 ] )
     u_FileName = os.path.join( u_ToShow, "user.txt" )
-    with open( u_FileName ) as f:
-        u_Info = [ ]
-        for line in f:
-            line.rstrip( )
-            lineInfo = line.split( "=", 1 )
-            if len( lineInfo ) != 2: break;     # Skip this line if it doesnt have what we need
-            u_Info.append( "{} => {}".format( lineInfo[ 0 ], lineInfo[ 1 ] ) )
 
-    return """
-<div class="matelook_user_details">
-%s
-</div>
-""" % ( u_Info )
+    if not os.path.isfile( u_FileName ):
+        return "<p>Invalid username %s</p>" % escape( user_name )
+
+    with open( u_FileName ) as f:
+        u_Info = defaultdict( )
+        for line in f:
+            line = line.rstrip( )   # Chomp
+            lineInfo = line.split( "=", 1 ) # Split on '='
+            if len( lineInfo ) != 2: break;     # Skip this line if it doesnt have what we need
+
+            if lineInfo[ 0 ] == "mates":
+                lineInfo[ 1 ] = re.sub( r'(\[|\])', '', lineInfo[ 1 ] )
+                # mates = re.findall( r'\s*([^\s]*),*\s*', lineInfo[ 1 ] )
+                mates = lineInfo[ 1 ].split( ', ' )
+                mates.pop( )    # Remove last useless element
+                # print( mates )
+                # print( type( mates ) )
+                # for person in iter( mates ):
+                    # print( "Person: " + person )
+
+                u_Info[ lineInfo[ 0 ] ] = mates
+            else:
+                u_Info[ lineInfo[ 0 ] ] = lineInfo[ 1 ]
+
+    #print( u_Info );
+    imgLoc = os.path.join( u_ToShow, "profile.jpg" )
+    if not os.path.isfile( imgLoc ):
+        imgLoc = None
+        #imgLoc = url_for( 'static', filename='imgLoc' )
+    # else:
+        #imgLoc = url_for( 'static', filename='imgLoc' )
+
+    return render_template( "user.html", username=username[ 0 ], uInfo=u_Info, img=imgLoc )
+#     return """
+# <div class="matelook_user_details">
+# %s
+# </div>
+# """ % ( u_Info )
 
 
 @app.route('/login', methods=['GET', 'POST'])
