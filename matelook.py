af8764821219cff948b01ceba6f900027bda9ea0
Je <kkburst@gmail.com>
2016-10-26 10:00:08 +1100

    Changed host IP to 0.0.0.0

--- matelook.py
+++ matelook.py
@@ -1112,4 +1112,4 @@ def parseDataset( ):
 main( )
 
 if __name__ == "__main__":
-    app.run( debug=True, port=5000, host="127.0.0.1")#, threaded=True) # 0.0.0.0
+    app.run( debug=True, port=5000, host="0.0.0.0")#, threaded=True) # 0.0.0.0
